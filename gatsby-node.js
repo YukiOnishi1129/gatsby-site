bb5c3ad0492ee90f941549e499f2dc10c372d50f
yuki <yuki.onishi1129@gmail.com>
2021-01-17 01:30:44 +0900

    setting links for previous and next articles

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,6 +16,14 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
             id
             slug
           }
+          next {
+            title
+            slug
+          }
+          previous {
+            title
+            slug
+          }
         }
       }
     }
@@ -26,13 +34,17 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-  blogresult.data.allContentfulBlogPost.edges.forEach(({ node }) => {
-    createPage({
-      path: `/blog/post/${node.slug}`,
-      component: path.resolve(`./src/templates/blogpost-template.js`),
-      context: {
-        id: node.id,
-      },
-    })
-  })
+  blogresult.data.allContentfulBlogPost.edges.forEach(
+    ({ node, next, previous }) => {
+      createPage({
+        path: `/blog/post/${node.slug}`,
+        component: path.resolve(`./src/templates/blogpost-template.js`),
+        context: {
+          id: node.id,
+          next,
+          previous,
+        },
+      })
+    }
+  )
 }
