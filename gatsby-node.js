e657b13bc7b2b9e60c413ab8473d0979443f485f
yuki <yuki.onishi1129@gmail.com>
2021-01-17 02:53:10 +0900

    add: pagenation

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,7 +37,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   blogresult.data.allContentfulBlogPost.edges.forEach(
     ({ node, next, previous }) => {
       createPage({
-        path: `/blog/post/${node.slug}`,
+        path: `/blog/post/${node.slug}/`,
         component: path.resolve(`./src/templates/blogpost-template.js`),
         context: {
           id: node.id,
@@ -47,4 +47,22 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       })
     }
   )
+
+  const blogPostsPerPage = 6 // 1ページに表示する記事の数
+  const blogPosts = blogresult.data.allContentfulBlogPost.edges.length // 記事の数
+  const blogPages = Math.ceil(blogPosts / blogPostsPerPage) // 記事一覧ページの総数
+
+  Array.from({ length: blogPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog/` : `/blog/${i + 1}`,
+      component: path.resolve("./src/templates/blog-template.js"),
+      context: {
+        skip: blogPostsPerPage * i,
+        limit: blogPostsPerPage,
+        currentPage: i + 1, // 現在のページ番号,
+        isFirst: i + 1 === 1, // 最初のページ
+        isLast: i + 1 === blogPages, // 最後のページ
+      },
+    })
+  })
 }
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,11 +3,16 @@ import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons"
 
 /**
  * ブログ一覧ページ
  */
-export default ({ data, location }) => (
+export default ({ data, location, pageContext }) => (
   <Layout>
     <SEO
       pagetitle="ブログ"
@@ -33,6 +38,31 @@ export default ({ data, location }) => (
             </article>
           ))}
         </div>
+        <ul className="pagenation">
+          {!pageContext.isFirst && (
+            <li className="prev">
+              <Link
+                to={
+                  pageContext.currentPage === 2
+                    ? `/blog/`
+                    : `/blog/${pageContext.currentPage - 1}/`
+                }
+                rel="prev"
+              >
+                <FontAwesomeIcon icon={faChevronLeft} />
+                <span>前のページ</span>
+              </Link>
+            </li>
+          )}
+          {!pageContext.isLast && (
+            <li className="next">
+              <Link to={`/blog/${pageContext.currentPage + 1}/`} rel="next">
+                <span>次のページ</span>
+                <FontAwesomeIcon icon={faChevronRight} />
+              </Link>
+            </li>
+          )}
+        </ul>
       </div>
     </section>
   </Layout>
@@ -42,8 +72,12 @@ export default ({ data, location }) => (
  * クエリ
  */
 export const query = graphql`
-  query {
-    allContentfulBlogPost(sort: { fields: publishDate, order: DESC }) {
+  query($skip: Int!, $limit: Int!) {
+    allContentfulBlogPost(
+      sort: { fields: publishDate, order: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
       edges {
         node {
           title
