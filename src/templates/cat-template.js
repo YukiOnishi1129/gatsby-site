5fd51486973a8d5d757844960f0e0b6782075238
yuki <yuki.onishi1129@gmail.com>
2021-01-17 16:37:04 +0900

    add: category pagenation

--- src/templates/cat-template.js
+++ src/templates/cat-template.js
@@ -44,8 +44,10 @@ export default ({ data, location, pageContext }) => (
               <Link
                 to={
                   pageContext.currentPage === 2
-                    ? `/blog/`
-                    : `/blog/${pageContext.currentPage - 1}/`
+                    ? `/cat/${pageContext.catslug}`
+                    : `/cat/${pageContext.catslug}/${
+                        pageContext.currentPage - 1
+                      }/`
                 }
                 rel="prev"
               >
@@ -56,7 +58,12 @@ export default ({ data, location, pageContext }) => (
           )}
           {!pageContext.isLast && (
             <li className="next">
-              <Link to={`/blog/${pageContext.currentPage + 1}/`} rel="next">
+              <Link
+                to={`/cat/${pageContext.catslug}/${
+                  pageContext.currentPage + 1
+                }/`}
+                rel="next"
+              >
                 <span>次のページ</span>
                 <FontAwesomeIcon icon={faChevronRight} />
               </Link>
